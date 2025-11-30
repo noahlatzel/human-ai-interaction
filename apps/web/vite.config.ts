78f2a3346be1f303f5d8a75a0b53a0988ceed658
Noah Latzel <noahjlatzel@gmail.com>
2025-11-30 22:40:39 +0100

    fix: revert changes in vite.config.ts

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,12 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: process.env.VITE_BASE_PATH ?? "/haii/",
   plugins: [react()],
-  server: {
-    proxy: {
-      '/haii/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-      },
-    },
-  },
 });
